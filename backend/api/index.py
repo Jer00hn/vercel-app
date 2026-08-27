98efe470d61e89faabbf843b5c199ce99eee9ae9
Jer00hn <82202493+Jer00hn@users.noreply.github.com>
2026-08-28 00:35:37 +0300

    Fix syntax error in print

--- backend/api/index.py
+++ backend/api/index.py
@@ -18,7 +18,7 @@ async def lifespan(app: FastAPI):
     # Получаем переменные окружения
     rest_url = os.getenv("UPSTASH_REDIS_REST_URL")
     rest_token = os.getenv("UPSTASH_REDIS_REST_TOKEN")
-    print(f"rest_url:{rest_url}"
+    print(f"rest_url:{rest_url}")
     if not rest_url or not rest_token:
         raise RuntimeError("UPSTASH_REDIS_REST_URL and UPSTASH_REDIS_REST_TOKEN are required")
     
